efb81d5d4c85eec77e36bf63b644867591be542e
stayasleep <virtuallyinfekted@gmail.com>
2017-07-01 00:57:01 -0700

    routing for home page on port 1337

--- socketio_chat/app.js
+++ socketio_chat/app.js
@@ -3,8 +3,14 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 1337;
 const server = app.listen(PORT);
-const io = requite("socket.io")(server);
+const io = require("socket.io")(server);
 
 //globals for now...could be database later on
 let users =[];
 let connection =[];
+
+//create routing for home page
+app.use(express.static(path.join(__dirname,'client')));
+
+
+
